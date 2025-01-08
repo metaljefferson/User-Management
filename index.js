5ba284661e9bc3c4b2d3068ae0cb96621def455d
Jefferson Sousa de Paiva <x362624@rede.sp>
2025-01-08 15:24:55 -0300

    iniciando o Json dos users

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-var nome = document.querySelector("#exampleInputName")
-var gender = document.querySelectorAll("#form-user-create [name=gender]:checked")
-var birth = document.querySelector("#exampleInputBirth")
-var country = document.querySelector("#exampleInputCountry")
-var email = document.querySelector("#exampleInputEmail")
-var password = document.querySelector("#exampleInputPassword")
-var photo = document.querySelector("#exampleInputFile")
-var admin = document.querySelector("#exampleInputAdmin")
-
 var fields = document.querySelectorAll("#form-user-create [name]")
 
-fields.forEach(function(field, index){
+var user = {}
 
-    console.log(field.name)
+fields.forEach(function (field, index) {
+
+
+    if (field.name == "gender") {
+
+        if (field.checked === true) {
+            user[field.name] = field.value
+        }
+
+    } else {
+        user[field.name] = field.value
+    }
 
 })
+console.log(user)
+// Operador logico && "E ao mesmo tempo" para duas condiçoes serem verdadeiras
+// 1 = e atribuir um valor , 2 == e comparação de dois valores e 3 === e compara valor e tipo de dado
+// Json = javascript Object Notation
+// Json utiliza {chave: valor, baseado em chave valor}

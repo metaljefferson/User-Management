60161c6eb4f3b0ea9ae684b8ff69c44669cb3ca5
Jefferson Sousa de Paiva <x362624@rede.sp>
2025-01-13 15:22:12 -0300

    adicionando events no formulario

--- index.js
+++ index.js
@@ -1,22 +1,66 @@
 var fields = document.querySelectorAll("#form-user-create [name]")
-
 var user = {}
 
-fields.forEach(function (field, index) {
+function addLine(dataUser) {
 
 
-    if (field.name == "gender") {
+    console.log(dataUser)
+    document.getElementById("table-users").innerHTML = `
+    <tr>
+        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+        <td>${dataUser.name}</td>
+        <td>${dataUser.email}</td>
+        <td>${dataUser.admin}</td>
+        <td>${dataUser.birth}</td>
+        <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+        </td>
+    </tr>`
 
-        if (field.checked === true) {
+
+
+}
+
+document.getElementById("form-user-create").addEventListener("submit", function (event) {
+
+    event.preventDefault()
+
+    fields.forEach(function (field, index) {
+
+
+        if (field.name == "gender") {
+
+            if (field.checked) {
+                user[field.name] = field.value
+            }
+
+        } else {
             user[field.name] = field.value
         }
 
-    } else {
-        user[field.name] = field.value
-    }
+    })
 
+    var objectUser = new User(
+        user.name,
+        user.gender,
+        user.birth,
+        user.country,
+        user.email,
+        user.password,
+        user.photo,
+        user.admin
+    )
+
+    addLine(objectUser)
 })
-console.log(user)
+
+
+
+
+
+
+// Um objeto e uma variavel que instancia(representa) uma classe
 // Operador logico && "E ao mesmo tempo" para duas condiçoes serem verdadeiras
 // 1 = e atribuir um valor , 2 == e comparação de dois valores e 3 === e compara valor e tipo de dado
 // Json = javascript Object Notation
